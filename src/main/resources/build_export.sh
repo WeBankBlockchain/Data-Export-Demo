d2ddfd93f80ec9363ca68830da01dfe2a73a009d
卡拉马佐夫 <973459080@qq.com>
2021-04-01 19:23:46 +0800

    service export

--- src/main/resources/build_export.sh
+++ src/main/resources/build_export.sh
@@ -19,6 +19,10 @@ function docker_install()
 
 docker_install
 
+CONFIGPATH="./config/application.properties"
+url=` ifconfig | grep "inet " | grep -v 127.0.0.1 | awk -F ' ' '{print $2}'`
+sed -i  "" "s/localhost/${url}/g" ${CONFIGPATH}
+sed -i  "" "s/127.0.0.1/${url}/g" ${CONFIGPATH}
 
 # @function: output information log
 # @param: content: information message
@@ -30,13 +34,15 @@ function LOG_INFO()
 
 BASE_DIR=`pwd`
 docker pull wangyue168git/dataexport:1.7.2
-docker run -d -p 5200:5200  -v "$BASE_DIR"/config/:/config -v "$BASE_DIR"/log/:/log --name export wangyue168git/dataexport:1.7.2
+docker run -d -p 5200:5200  -v "$BASE_DIR"/config/:/config -v "$BASE_DIR"/log/:/log --name dataexport wangyue168git/dataexport:1.7.2
 LOG_INFO "data export run success"
 
-v=`grep system.grafanaEnable ./config/application.properties | cut -d'=' -f2`
+v=`grep system.grafanaEnable ${CONFIGPATH} | cut -d'=' -f2`
 
 if [ ${v} == "true" ]; then
     docker pull grafana/grafana
     docker run   -d   -p 3000:3000   --name=grafana   -e "GF_INSTALL_PLUGINS=grafana-clock-panel,grafana-simple-json-datasource"   grafana/grafana
     LOG_INFO "grafana run success"
 fi
+
+LOG_INFO "See the logging command: docker logs -f dataexport"
